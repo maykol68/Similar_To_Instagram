68fd563c26efda836903aa53962a1f042348c378
maymine <sm9365277@gmail.com>
2023-11-14 20:36:52 -0500

    added sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }   
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

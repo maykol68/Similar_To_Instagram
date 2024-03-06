457eafdcf6f0d4e0380d234fbdf188d91eec7559
maykol68 <sm9365277@gmail.com>
2024-03-06 18:25:34 -0500

    Search controller Action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,26 @@
 class SearchController < ApplicationController
 
     def index
-        
+        @results = search_for_users
+
+        respond_to do |format|
+            format.turbo_stream do
+                render turbo_stream:
+                    turbo_stream.update("users",
+                                        partial: "users/show",
+                                        locals: { users: @results })
+            end
+        end
+    end
+
+    private
+
+    def search_for_users
+        if params[:query].blank?
+            User.all
+       else
+            User.search(params[:query], Fields: %[uusername name], operator: "or", match: :text_middle)
+       end
+end
     end
 end
--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,7 +1,6 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="search"
 export default class extends Controller {
-  connect() {
-  }
+  connect() {}
 }

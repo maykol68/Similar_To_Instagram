2a118f7488423598382559bb08d788f9cd1ea1ae
maykol68 <sm9365277@gmail.com>
2024-03-13 03:57:54 -0500

    Direct Message v2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,12 @@ class UsersController < ApplicationController
 
         @room = Room.new
         @rooms = Room.public_rooms
+        @room_name = get_name(@user, current_user)
+        @single_room = Room.where(name: @room_name).first || Room.create_private_room([@user, current_user], @room_name)
 
+        @message = Message.new
+        @messages = @single_room.messages.order(created_at: :asc)
+        render 'rooms/index'
 
         @pagy, @posts = pagy_countless(FindPosts.new.call({user_id: @user.id}).load_async, items: 12)   
     end
@@ -32,5 +37,12 @@ class UsersController < ApplicationController
 
       def set_user_follow
         @user_follow = User.find(params[:id])
+      end
+
+      private
+
+      def get_name(user1, user2)
+        user = [user1, user2].sort
+        "private_#{user[0].id}_#{user[1].id}"
       end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -8,7 +8,13 @@ class Room < ApplicationRecord
 
     def broadcast_if_public
         broadcast_append_to "rooms" unless self.is_private
-            
+    end
+
+    def self.create_private_room(users, room_name)
+        single_room = Room.create(name: room_name, is_private: true)
+        users.each do |user|
+            Participant.create(user_id: user.id, room_id: single_room.id)
         end
     end
+
 end

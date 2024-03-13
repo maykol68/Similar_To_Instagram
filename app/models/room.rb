24a2159d5c45309cdaf92322f38969755ba0acab
maykol68 <sm9365277@gmail.com>
2024-03-13 03:51:34 -0500

    Direct Message Model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,14 @@
 class Room < ApplicationRecord
     validates_uniqueness_of :name
     scope :public_rooms, -> {where(is_private: false)}
-    after_create_commit {broadcast_append_to "rooms"}
+    after_create_commit {broadcast_if_public }
 
     has_many :messages
+    has_many :participants, dependent: :destroy
+
+    def broadcast_if_public
+        broadcast_append_to "rooms" unless self.is_private
+            
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_13_065338) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_13_084148) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -92,6 +92,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_13_065338) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "participants", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_participants_on_room_id"
+    t.index ["user_id"], name: "index_participants_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -137,5 +146,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_13_065338) do
   add_foreign_key "likes", "users"
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "participants", "rooms"
+  add_foreign_key "participants", "users"
   add_foreign_key "posts", "users"
 end
